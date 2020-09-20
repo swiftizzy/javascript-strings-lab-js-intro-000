f3e53e80680d0c650f99296a0e91c1455f91c200
michael quinteros <mquinteros1026@gmail.com>
2020-09-20 00:22:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var greeting = "Hello,  everybody!";
+var greeting = "";
 
 var specialGuest = "Neil deGrasse Tyson"
 
@@ -7,3 +7,5 @@ var greetSpecialGuest = "" + specialGuest + "!";
 var topic = "space";
 
 var conversation = `${topic}`;
+
+greeting = "Hello, everybody!";
